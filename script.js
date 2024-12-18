f75a2fbff054a04de449b39743d5722cdad7188e
Tanmay Chachad <tanmaychachad@gmail.com>
2024-12-18 09:28:36 +0530

    Uploading Files

---
+++ script.js
@@ -0,0 +1,24 @@
+const number = document.getElementById("number");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const cross = document.getElementById("cross");
+
+var num = 0;
+
+function addition(){
+    num = parseInt(number.value) + 1;
+    number.value = num;
+}
+
+function subtraction(){
+    if(number.value>0)
+    {
+        num = parseInt(number.value) - 1;
+        number.value = num;
+    }
+}
+
+function resetZero(){
+    number.value = 0;
+    alert("Number is reset to '0'");
+}
